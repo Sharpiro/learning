6197d148f38f89655d0e823160c2065e343c6fa1
sharpiro <dsharpbb09@gmail.com>
2016-03-05 16:15:26 -0500

    solved coconut problem

--- InterviewPrep.DailyProgrammer/CoconutCounter.cs
+++ InterviewPrep.DailyProgrammer/CoconutCounter.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 
 namespace InterviewPrep.DailyProgrammer
 {
@@ -7,45 +6,34 @@ namespace InterviewPrep.DailyProgrammer
     {
         public int CountCocounuts(int numberOfSailors)
         {
-            var startingNuts = 3121;
-            var currentNuts = startingNuts;
-            //#OfSailors = 5;
-            //1 % n = 1;
-            for (var i = 0; i < 1; i++)
+            const int monkeyNuts = 1;
+            int numberOfCoconuts = -1;
+            bool breakOuterLoop;
+
+            for (var i = 2; i < int.MaxValue; i++)
             {
-                if (currentNuts % numberOfSailors != 1)
-                    throw new InvalidDataException();
-                var sailorNuts = (int)Math.Floor((double)currentNuts / numberOfSailors);
-                var monkeyNuts = 1;
-                SimpleMath1(currentNuts, sailorNuts, monkeyNuts);
-                SimpleMath2(currentNuts, sailorNuts, monkeyNuts);
-                SimpleMath3(currentNuts, sailorNuts, monkeyNuts);
-
+                breakOuterLoop = false;
+                var currentNuts = i;
+                for (var j = 0; j < numberOfSailors; j++)
+                {
+                    if (currentNuts % numberOfSailors != 1)
+                    {
+                        breakOuterLoop = true;
+                        break;
+                    }
+                    var sailorNuts = (int)Math.Floor((double)currentNuts / numberOfSailors);
+                    currentNuts = currentNuts - sailorNuts - monkeyNuts;
+                }
+                if (breakOuterLoop)
+                    continue;
+                var equalLeftOvers = currentNuts % numberOfSailors == 0;
+                if (equalLeftOvers)
+                {
+                    numberOfCoconuts = i;
+                    break;
+                }
             }
-            return -1;
-        }
-
-        private void SimpleMath1(int currentNuts, int sailorNuts, int monkeyNuts)
-        {
-            currentNuts = currentNuts - sailorNuts - monkeyNuts;
-            Console.WriteLine(currentNuts);
-        }
-
-        private void SimpleMath2(int currentNuts, int sailorNuts, int monkeyNuts)
-        {
-            currentNuts -= sailorNuts;
-            currentNuts -= monkeyNuts;
-            Console.WriteLine(currentNuts);
-        }
-
-        private void SimpleMath3(int currentNuts, int sailorNuts, int monkeyNuts)
-        {
-            // x = 5 + -3 + 1;
-            //x = 5 - 3;
-            //x = x - 1;
-            //x = 3121 + -624 + -1
-            currentNuts -= sailorNuts + monkeyNuts;
-            Console.WriteLine(currentNuts);
+            return numberOfCoconuts;
         }
     }
 }
