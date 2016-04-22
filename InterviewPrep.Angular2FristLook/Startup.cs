34a2a8bac3efd2dd89bc2772f7635f0bf325031a
sharpiro <dsharpbb09@gmail.com>
2016-04-22 13:04:16 -0400

    Finished OdeToFood AspnetCore Starting on Angular 2 First look

---
+++ InterviewPrep.Angular2FristLook/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
+
+namespace InterviewPrep.Angular2FristLook
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IApplicationEnvironment env)
+        {
+            app.UseIISPlatformHandler();
+
+            app.UseNodeModules(env);
+
+            app.UseFileServer();
+        }
+
+        // Entry point for the application.
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
