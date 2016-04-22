33123b3909b3980d5904c8969cacf13843e0468d
sharpiro <dsharpbb09@gmail.com>
2016-04-22 16:08:21 -0400

    adding what VS missed...

---
+++ InterviewPrep.Angular2FristLook/wwwroot/app/appComponent.ts
@@ -0,0 +1,18 @@
+﻿import {Component} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
+import {VehicleService} from "./shared/vehicleService"
+import {DashboardComponent} from "./dashboard/dashboardComponent"
+
+@Component({
+    selector: "my-app",
+    directives: [ROUTER_DIRECTIVES],
+    providers: [VehicleService, ROUTER_PROVIDERS],
+    template: "<router-outlet></router-outlet>"
+})
+@RouteConfig([
+    { path: '/dashboard', name: 'Dashboard', component: DashboardComponent, useAsDefault: true },
+])
+export class AppComponent
+{
+
+}
