e1cdff879936a2ee30765719abc0c85712fb6cda
sharpiro <dsharpbb09@gmail.com>
2016-04-22 16:08:21 -0400

    adding what VS missed...

---
+++ InterviewPrep.Angular2FristLook/wwwroot/app/dashboard/dashboardComponent.ts
@@ -0,0 +1,16 @@
+﻿import {Component} from "angular2/core";
+import {VehicleService} from "../shared/vehicleService"
+
+@Component({
+    selector: "my-app",
+    templateUrl: "/app/dashboard/dashboardComponent.html",
+})
+export class DashboardComponent
+{
+    public vehicles: Array<IBaseData>;
+
+    constructor(private dataService: VehicleService)
+    {
+        this.vehicles = dataService.getVehicles();
+    }
+}
