b4d83091402cb65503b91f651153789ed382899d
sharpiro <dsharpbb09@gmail.com>
2016-04-24 15:05:08 -0400

    making "typings" adjustments to .gitignore

--- InterviewPrep.Angular2FristLook/wwwroot/app/main.ts
+++ InterviewPrep.Angular2FristLook/wwwroot/app/main.ts
@@ -1,6 +1,4 @@
-﻿/// <reference path="../../node_modules/angular2/typings/browser.d.ts" />
-
-import {bootstrap}    from "angular2/platform/browser";
+﻿import {bootstrap}    from "angular2/platform/browser";
 import {AppComponent} from "./appComponent";
 
 bootstrap(AppComponent); 
