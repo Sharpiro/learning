621f381fce139c73b2f4c0f15288058ca0cc81f9
sharpiro <dsharpbb09@gmail.com>
2016-04-27 20:19:09 -0400

    updated dependencies except rxjs due to errors

--- InterviewPrep.Angular2FirstLook/wwwroot/app/main.ts
+++ InterviewPrep.Angular2FirstLook/wwwroot/app/main.ts
@@ -1,7 +1,8 @@
 ﻿import {bootstrap}    from "angular2/platform/browser";
 import {provide} from "angular2/core"
 import {AppComponent} from "./appComponent";
-import {ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy, APP_BASE_HREF, PathLocationStrategy} from "angular2/router"
+import {ROUTER_PROVIDERS} from "angular2/router"
+import {LocationStrategy, HashLocationStrategy, APP_BASE_HREF, PathLocationStrategy} from "angular2/platform/common"
 
 bootstrap(AppComponent, [ROUTER_PROVIDERS,
     provide(APP_BASE_HREF, { useValue: '/' }),
