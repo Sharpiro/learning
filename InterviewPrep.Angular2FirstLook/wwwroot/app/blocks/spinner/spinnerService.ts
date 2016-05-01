d5d3e5d2b2e4999d5922efcda805ea55271bc343
sharpiro <dsharpbb09@gmail.com>
2016-05-01 15:29:47 -0400

    starting on spinner code

---
+++ InterviewPrep.Angular2FirstLook/wwwroot/app/blocks/spinner/spinnerService.ts
@@ -0,0 +1,25 @@
+﻿import { Injectable } from "angular2/core";
+import { Observable, Subject } from "rxjs/Rx";
+
+export interface ISpinnerState
+{
+    show: boolean
+}
+
+@Injectable()
+export class SpinnerService
+{
+    private spinnerSubject: Subject<ISpinnerState> = new Subject();
+
+    public spinnerState = this.spinnerSubject.asObservable();
+
+    show()
+    {
+        this.spinnerSubject.next(<ISpinnerState>{ show: true });
+    }
+
+    hide()
+    {
+        this.spinnerSubject.next(<ISpinnerState>{ show: false });
+    }
+}
