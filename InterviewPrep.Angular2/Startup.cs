a87463f5b601e360327c030ff2c7ca06186d8bd7
sharpiro <dsharpbb09@gmail.com>
2016-05-25 15:48:37 -0400

    Refactored angular 2 app into new project due to new tooling removed RC1 version added script allowing compatibility with older browsers (IE LUL) Unloaded un-converted projects for now

--- InterviewPrep.Angular2/Startup.cs
+++ InterviewPrep.Angular2/Startup.cs
@@ -1,23 +1,22 @@
 ﻿using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.PlatformAbstractions;
 
-namespace InterviewPrep.Angular2FirstLook
+namespace InterviewPrep.Angular2
 {
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IApplicationEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseIISPlatformHandler();
-
             app.UseRequestMiddleware();
 
             app.UseMvc(options => { options.MapRoute("DefaultApi", "api/{controller}/{action}/{id?}"); });
@@ -26,8 +25,5 @@ namespace InterviewPrep.Angular2FirstLook
 
             app.UseFileServer();
         }
-
-        // Entry point for the application.
-        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
     }
 }
