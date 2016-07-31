c528f0ffac3fc18c1f1279be5c52f292ef1d7a19
sharpiro <dsharpbb09@gmail.com>
2016-07-31 13:18:36 -0400

    Updated Queue and Stack

--- InterviewPrep.CoreTests/Lists/QueueTests.cs
+++ InterviewPrep.CoreTests/Lists/QueueTests.cs
@@ -1,18 +1,61 @@
-﻿//using InterviewPrep.Core.Lists.SingleLinkedList;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using InterviewPrep.Core.Lists.SingleLinkedList;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
 
-//namespace InterviewPrep.CoreTests.Lists
-//{
-//    public class QueueTests
-//    {
-//        [TestMethod]
-//        public void QueueSllTest()
-//        {
-//            var queue = new QueueSll<int>();
-//            queue.Enqueue(0);
-//            queue.Enqueue(1);
-//            var value = queue.Dequeue();
-//            Assert.AreEqual(value, 0);
-//        }
-//    }
-//}
+namespace InterviewPrep.CoreTests.Lists
+{
+    [TestClass]
+    public class QueueTests
+    {
+        [TestMethod]
+        public void SimpleTest()
+        {
+            var queue = new CustomQueue();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            var value = queue.Dequeue();
+            Assert.AreEqual(value, 1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void DequeueEmptyTest()
+        {
+            var queue = new CustomQueue();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            var value = queue.Dequeue();
+            var value2 = queue.Dequeue();
+            Assert.AreEqual(value, 1);
+            Assert.AreEqual(value2, 2);
+            queue.Dequeue();
+        }
+
+        [TestMethod]
+        public void Simple2Test()
+        {
+            var queue = new CustomQueue();
+            for (var i = 0; i < 50; i++)
+            {
+                queue.Enqueue(i);
+            }
+            var value = queue.Dequeue();
+            Assert.AreEqual(value, 0);
+            Assert.AreEqual(49, queue.Length);
+        }
+
+        [TestMethod]
+        public void LengthTest()
+        {
+            var queue = new CustomQueue();
+            for (var i = 0; i < 50; i++)
+            {
+                queue.Enqueue(i);
+            }
+            var list = queue.ToList();
+            Assert.AreEqual(50, queue.Length);
+            Assert.AreEqual(50, list.Count);
+        }
+    }
+}
---
+++ InterviewPrep.CoreTests/Lists/StackTests.cs
@@ -0,0 +1,63 @@
+﻿using InterviewPrep.Core.Lists.SingleLinkedList;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace InterviewPrep.CoreTests.Lists
+{
+    [TestClass]
+    public class StackTests
+    {
+        [TestMethod]
+        public void SimpleTest()
+        {
+            var stack = new CustomStack();
+            stack.Push(1);
+            stack.Push(2);
+            var value = stack.Pop();
+            var value2 = stack.Pop();
+            Assert.AreEqual(value, 2);
+            Assert.AreEqual(value2, 1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void PopEmptyTest()
+        {
+            var stack = new CustomStack();
+            stack.Push(1);
+            stack.Push(2);
+            var value = stack.Pop();
+            var value2 = stack.Pop();
+            Assert.AreEqual(value2, 1);
+            Assert.AreEqual(value2, 1);
+            stack.Pop();
+        }
+
+        [TestMethod]
+        public void Simple2Test()
+        {
+            var stack = new CustomStack();
+            for (var i = 0; i < 50; i++)
+            {
+                stack.Push(i);
+            }
+            var value = stack.Pop();
+            Assert.AreEqual(value, 49);
+            Assert.AreEqual(49, stack.Length);
+        }
+
+        [TestMethod]
+        public void LengthTest()
+        {
+            var stack = new CustomStack();
+            for (var i = 0; i < 50; i++)
+            {
+                stack.Push(i);
+            }
+            var list = stack.ToList();
+            Assert.AreEqual(50, stack.Length);
+            Assert.AreEqual(50, list.Count);
+        }
+    }
+}
