38daf9d87a26ebd71d27b2924aa684aa132282df
sharpiro <dsharpbb09@gmail.com>
2016-08-27 14:24:09 -0400

    added linq 2 xml

---
+++ src/InterviewPrep.LinqFundamentals/XmlHelper.cs
@@ -0,0 +1,34 @@
+﻿using InterviewPrep.LinqFundamentals.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace InterviewPrep.LinqFundamentals
+{
+    public class XmlHelper
+    {
+        private readonly IReadOnlyCollection<Car> _records;
+
+        public XmlHelper(IReadOnlyCollection<Car> records)
+        {
+            _records = records;
+        }
+
+        public XDocument CreateXDocument()
+        {
+            var document = new XDocument(new XElement("Cars",
+                _records.Select(r => new XElement("Car",
+                    new XAttribute("Name", r.Model),
+                    new XAttribute("Combined", r.Combined),
+                    new XAttribute("Manufacturer", r.Make)
+                ))));
+            return document;
+        }
+
+        public T ParseXML<T>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/InterviewPrep.Pluralsight.Core.Tests/LinqFundamentals/XmlHelperTests.cs
@@ -0,0 +1,52 @@
+﻿using InterviewPrep.LinqFundamentals;
+using InterviewPrep.LinqFundamentals.Models;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using Xunit;
+
+namespace InterviewPrep.Pluralsight.Core.Tests.LinqFundamentals
+{
+    public class XmlHelperTests
+    {
+        private readonly string _fileDirectory;
+        private readonly IReadOnlyList<Car> _carData;
+        private readonly IReadOnlyList<Manufacturer> _makeData;
+
+        public XmlHelperTests()
+        {
+            _fileDirectory = $"{Directory.GetCurrentDirectory()}/LinqFundamentals/Data";
+            _carData = File.ReadAllLines($"{_fileDirectory}/fuel.csv").ParseCsv<Car>().ToList();
+            _makeData = File.ReadAllLines($"{_fileDirectory}/manufacturers.csv").ParseCsv<Manufacturer>().ToList();
+        }
+
+        [Fact]
+        public void CreateXDocumentTest()
+        {
+            var xmlHelper = new XmlHelper(_carData);
+            var data = xmlHelper.CreateXDocument();
+            Assert.False(string.IsNullOrEmpty(data.ToString()));
+        }
+
+        [Fact]
+        public void QueryXDocumentTest()
+        {
+            var xmlHelper = new XmlHelper(_carData);
+            var xdata = xmlHelper.CreateXDocument();
+            var car = xdata.Element("Cars").Elements("Car")
+                .Where(d => d.Attribute("Manufacturer").Value.ToLowerInvariant() == "bmw");
+
+            using (var reader = new StringReader(xdata.ToString()))
+            {
+                using (var xReader = XmlReader.Create(reader))
+                {
+                    while (xReader.Read())
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+}
