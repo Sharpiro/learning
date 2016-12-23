e2846a9bae86d004d02283756f82d28119b8bbcd
sharpiro <dsharpbb09@gmail.com>
2016-12-23 11:02:07 -0500

    added a few inheritance tests

---
+++ InterviewPrep.Core/Inheritance/Inheritance.cs
@@ -0,0 +1,28 @@
+﻿namespace InterviewPrep.Core.Inheritance
+{
+    public class Base
+    {
+        public virtual int GetOverride()
+        {
+            return 0;
+        }
+
+        public virtual int GetNew()
+        {
+            return 0;
+        }
+    }
+
+    public class ChildOne : Base
+    {
+        public override int GetOverride()
+        {
+            return 1;
+        }
+
+        public new int GetNew()
+        {
+            return 1;
+        }
+    }
+}
---
+++ InterviewPrep.CoreTests/InheritanceTests.cs
@@ -0,0 +1,31 @@
+﻿using InterviewPrep.Core.Inheritance;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InterviewPrep.CoreTests
+{
+    [TestClass]
+    public class InheritanceTests
+    {
+        [TestMethod]
+        public void OverrideAndNewTest()
+        {
+            Base @base = new Base();
+            Base childX = new ChildOne();
+            var childY = new ChildOne();
+
+            var baseOverrideResult = @base.GetOverride();
+            var childXOverrideResult = childX.GetOverride();
+            var childYOverrideResult = childY.GetOverride();
+            var baseNewResult = @base.GetNew();
+            var childXNewResult = childX.GetNew();
+            var childYNewResult = childY.GetNew();
+
+            Assert.AreEqual(0, baseOverrideResult);
+            Assert.AreEqual(1, childXOverrideResult);
+            Assert.AreEqual(1, childYOverrideResult);
+            Assert.AreEqual(0, baseNewResult);
+            Assert.AreEqual(0, childXNewResult);
+            Assert.AreEqual(1, childYNewResult);
+        }
+    }
+}
