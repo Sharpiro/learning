779ec45c9e6d07da0b012ccfe0662cb43de3ca1c
sharpiro <dsharpbb09@gmail.com>
2016-12-29 15:55:16 -0500

    adding rolyn section

---
+++ RoslynTests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using System.Linq;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace RoslynTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            const string source = "class Test {}";
+            var script = CSharpScript.Create(source);
+            var compilaton = script.GetCompilation().SyntaxTrees.Single().GetCompilationUnitRoot();
+            var descendants = compilaton.DescendantNodesAndTokens().ToList();
+        }
+    }
+}
