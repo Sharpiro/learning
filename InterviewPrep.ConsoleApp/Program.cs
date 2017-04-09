a396ef545a4c69aa15abdb844f057f537330432c
sharpiro <dsharpbb09@gmail.com>
2017-04-09 12:21:49 -0400

    hack fix of build issues

--- InterviewPrep.ConsoleApp/Program.cs
+++ InterviewPrep.ConsoleApp/Program.cs
@@ -1,25 +1,11 @@
-﻿using InterviewPrep.Core;
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-
+﻿
 namespace InterviewPrep.ConsoleApp
 {
     public static class Program
     {
-        private static ElevatorSystem _elevatorSystem = new ElevatorSystem();
-
         public static void Main()
         {
-            var floors = new List<int> { 5, 4, 9, 2, 7, 11, 22, 5 };
-            var task = _elevatorSystem.Run(floors);
-            //var task = _elevatorSystem.Run();
-            //while (true)
-            //{
-            //    var floor = Convert.ToInt32(Console.ReadLine());
-            //    _elevatorSystem.QueueFloor(floor);
-            //}
-            Console.ReadLine();
+
         }
     }
 }
