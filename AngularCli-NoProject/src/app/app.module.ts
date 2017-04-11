52f14a525854bf04fc6fe0f1a6cff6f8020a0ec5
sharpiro <dsharpbb09@gmail.com>
2017-04-11 16:47:35 -0400

    updated boilerplate

---
+++ AngularCli-NoProject/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CustomerComponent } from './customer/customer.component';
+import { AdminModule } from 'app/admin/admin.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    CustomerComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AdminModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
