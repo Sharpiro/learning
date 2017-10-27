3de016c85909abfb310038d0e5612b30852c24e1
sharpiro <dsharpbb09@gmail.com>
2017-10-27 13:53:44 -0400

    added bouncer library

--- Learning/src/main.ino
+++ Learning/src/main.ino
@@ -1,24 +1,11 @@
+#include <Bounce2.h>
 #include <Arduino.h>
 
 const int pinFive = 5;
-int readingValue = LOW;
+int ledValue = LOW;
+Bounce bouncer = Bounce();
 
 extern HardwareSerial Serial;
-
-byte receiveBytes(byte *data)
-{
-    auto availableBytes = Serial.available();
-    if (!availableBytes)
-        return 0;
-    data = (byte *)malloc(sizeof(byte) * availableBytes);
-    for (auto i = 0; i < availableBytes; i++)
-    {
-        auto reading = (byte)Serial.read();
-        Serial.print(reading);
-        data[i] = reading;
-    }
-    return availableBytes;
-}
 
 void setup()
 {
@@ -27,32 +14,17 @@ void setup()
     // pinMode(pinFive, INPUT);
     pinMode(pinFive, INPUT_PULLUP);
     pinMode(LED_BUILTIN, OUTPUT);
+    bouncer.attach(pinFive);
 }
 
 void loop()
 {
-    byte *data;
-    auto dataLength = receiveBytes(data);
-    for (auto i = 0; i < dataLength; i++)
+    if (bouncer.update()) 
     {
-        Serial.print(data[i]);
-    }
-    if (dataLength)
-        Serial.println();
-    delete (data);
-    auto reading = !digitalRead(pinFive);
-    if (reading == HIGH && readingValue != HIGH)
-    {
-        readingValue = HIGH;
-        Serial.println(reading);
-        digitalWrite(LED_BUILTIN, readingValue);
-        delay(500);
-    }
-    else if (reading == LOW && readingValue != LOW)
-    {
-        readingValue = LOW;
-        Serial.println(reading);
-        digitalWrite(LED_BUILTIN, readingValue);
-        delay(500);
+        if (bouncer.read() == LOW)
+        {
+            ledValue = !ledValue;
+            digitalWrite(LED_BUILTIN, ledValue);
+        }
     }
 }
