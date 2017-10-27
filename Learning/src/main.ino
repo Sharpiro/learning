e0a4e33fd27e4724b920d4d2c7d3b50aa9a0ccb9
sharpiro <dsharpbb09@gmail.com>
2017-10-27 15:29:57 -0400

    reading data from flash

--- Learning/src/main.ino
+++ Learning/src/main.ino
@@ -1,31 +1,32 @@
 #include <Arduino.h>
 
-const int interruptPin = 2;
 const int pinFive = 5;
 int ledValue = LOW;
 
 extern HardwareSerial Serial;
 
-void beEratic()
-{
-    Serial.println("changed everything");
-}
+const char maxLength = 5;
+char sequence[maxLength];
+
+PROGMEM const char letters[26][maxLength] = {
+    ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..",   // A-I
+    ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", // J-R
+    "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."         // S-Z
+};
 
 void setup()
 {
     Serial.begin(9600);
     Serial.println("starting");
     // pinMode(pinFive, INPUT);
-    pinMode(interruptPin, INPUT_PULLUP);
-    pinMode(LED_BUILTIN, OUTPUT);
-    auto analogReading = analogRead(0);
-    randomSeed(analogReading);
-    attachInterrupt(0, beEratic, CHANGE);
+    // pinMode(LED_BUILTIN, OUTPUT);
+    randomSeed(analogRead(0));
 }
 
 void loop()
 {
-    auto number = random(1, 10);
-    Serial.println(number);
-    delay(1000);
+    auto randomIndex = random(0, 26);
+    memcpy_P(sequence, letters[randomIndex], maxLength);
+    Serial.println(sequence);
+    delay(2000);
 }
