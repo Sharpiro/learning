77a53187bbedfb6b8154c5402cf8a432de6c328b
sharpiro <dsharpbb09@gmail.com>
2017-11-04 16:38:55 -0400

    adding chatpad code

--- Learning/programming_arduino/src/main.ino
+++ Learning/programming_arduino/src/main.ino
@@ -1,37 +1,36 @@
 #include <Arduino.h>
 #include "MemoryFree.h"
+#include "Chatpad.h"
 
-const int pinFive = 5;
-int ledValue = LOW;
+// const int pinFive = 5;
 extern HardwareSerial Serial;
+int ledState = 0;
+Chatpad pad;
+
+void print_keys(Chatpad &pad, Chatpad::keycode_t code, Chatpad::eventtype_t type)
+{
+    if (type == Chatpad::Down)
+    {
+        char a = pad.toAscii(code);
+        if (a != 0)
+        {
+            ledState = !ledState;
+            digitalWrite(LED_BUILTIN, HIGH);
+            // Serial.print(a);
+        }
+    }
+}
 
 void setup()
 {
-    Serial.begin(9600);
-    Serial.println("starting");
-    pinMode(pinFive, OUTPUT);
+    // Serial.begin(57600);
+    // Serial.println("starting");
+    pad.init(Serial, print_keys);
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, LOW);
-
-    Serial.print("free memory: ");
-    Serial.println(freeMemory());
 }
 
 void loop()
 {
-    if (Serial.available())
-    {
-        char input = Serial.read();
-
-        if (input == '1')
-        {
-            digitalWrite(pinFive, HIGH);
-            Serial.println("turning power on...");
-        }
-        else if (input == '0')
-        {
-            digitalWrite(pinFive, LOW);
-            Serial.println("turning power off...");
-        }
-    }
+    pad.poll();
 }
