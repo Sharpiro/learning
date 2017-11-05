47b32a375d387ba698759b92459b95d6b3169212
sharpiro <dsharpbb09@gmail.com>
2017-11-05 08:49:01 -0500

    adding lcd lib

--- Learning/programming_arduino/src/main.ino
+++ Learning/programming_arduino/src/main.ino
@@ -1,36 +1,44 @@
 #include <Arduino.h>
-#include "MemoryFree.h"
-#include "Chatpad.h"
+#include <LiquidCrystal.h>
 
 // const int pinFive = 5;
 extern HardwareSerial Serial;
-int ledState = 0;
-Chatpad pad;
-
-void print_keys(Chatpad &pad, Chatpad::keycode_t code, Chatpad::eventtype_t type)
-{
-    if (type == Chatpad::Down)
-    {
-        char a = pad.toAscii(code);
-        if (a != 0)
-        {
-            ledState = !ledState;
-            digitalWrite(LED_BUILTIN, HIGH);
-            // Serial.print(a);
-        }
-    }
-}
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+int numRows = 2;
+int numColumns = 16;
+// int count = 0;
+const char message[] = "sup witches";
+int messageLength = 11;
+int index = messageLength - 1;
 
 void setup()
 {
-    // Serial.begin(57600);
-    // Serial.println("starting");
-    pad.init(Serial, print_keys);
-    pinMode(LED_BUILTIN, OUTPUT);
-    digitalWrite(LED_BUILTIN, LOW);
+    Serial.begin(9600);
+    Serial.println("starting");
+    lcd.begin(numRows, numColumns);
+    lcd.clear();
+    // lcd.setCursor(0, 0);
+    // lcd.print("sup");
+    // lcd.setCursor(0, 1);
+    // lcd.print("witches");
+    // pinMode(LED_BUILTIN, OUTPUT);
+    // digitalWrite(LED_BUILTIN, LOW);
 }
 
 void loop()
 {
-    pad.poll();
+    if (index < 0)
+    {
+        lcd.clear();
+        index = messageLength - 1;
+        delay(1000);
+    }
+    lcd.setCursor(index, 0);
+    lcd.print(message[index]);
+    index--;
+    //time
+    // lcd.setCursor(0, 1);
+    // lcd.print(++count);
+    if (message[index] != ' ')
+        delay(500);
 }
