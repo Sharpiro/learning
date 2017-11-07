9d09538a0f75d94244c7b55f2bf4cefa2d8c2780
sharpiro <dsharpbb09@gmail.com>
2017-11-07 17:32:11 -0500

    progress on functions

--- extensions/JsonEditorLocal/index.js
+++ extensions/JsonEditorLocal/index.js
@@ -1,17 +1,52 @@
 import JSONEditor from "jsoneditor";
 import { JSONEditorMode, JSONEditorOptions } from "jsoneditor";
 
-var codeContainer = document.getElementById("jsonEditorCode");
-var viewContainer = document.getElementById("jsonEditorView");
-var jsonEditorCode = new JSONEditor(codeContainer, { mode: "code" });
-var jsonEditorView = new JSONEditor(viewContainer, { mode: "view" });
+var leftEditorContainer = document.getElementById("leftEditorContainer");
+var rightEditorContainer = document.getElementById("rightEditorContainer");
+var body = document.getElementById("body");
+var leftJsonEditor = new JSONEditor(leftEditorContainer, { mode: "code" });
+var rightJsonEditor = new JSONEditor(rightEditorContainer, { mode: "form" });
 
 var data = { data: 12 };
-jsonEditorCode.set(data);
-jsonEditorView.set(data);
+leftJsonEditor.set(data);
+rightJsonEditor.set(data);
 
 updateButton.onclick = () => {
-    var newJson = jsonEditorCode.get(data);
-    jsonEditorCode.set(newJson);
-    jsonEditorView.set(newJson);
+    var newJson = leftJsonEditor.get(data);
+    leftJsonEditor.set(newJson);
+    rightJsonEditor.set(newJson);
+}
+
+updateTreeButton.onclick = () => {
+    var newJson = rightJsonEditor.get(data);
+    leftJsonEditor.set(newJson);
+    rightJsonEditor.set(newJson);
+}
+
+swapToFormButton.onclick = () => {
+    rightJsonEditor.setMode("form");
+}
+
+swapToTreeButton.onclick = () => {
+    rightJsonEditor.setMode("tree");
+}
+
+leftEditorContainer.onkeydown = (ev) => {
+    if (ev.ctrlKey && ev.keyCode === 83) {
+        ev.preventDefault();
+        var newJson = leftJsonEditor.get(data);
+        leftJsonEditor.set(newJson);
+        rightJsonEditor.set(newJson);
+        console.log(ev);
+    }
+}
+
+rightEditorContainer.onkeydown = (ev) => {
+    if (ev.ctrlKey && ev.keyCode === 83) {
+        ev.preventDefault();
+        var newJson = rightJsonEditor.get(data);
+        leftJsonEditor.set(newJson);
+        rightJsonEditor.set(newJson);
+        console.log(ev);
+    }
 }
