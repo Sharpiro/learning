c1c617494c87800a348af1120dbe4a56ebb789c3
sharpiro <dsharpbb09@gmail.com>
2020-03-27 23:11:28 -0400

    manually call iterator from js

--- prototypes/wasm-game-of-life/src/lib.rs
+++ prototypes/wasm-game-of-life/src/lib.rs
@@ -15,67 +15,31 @@ macro_rules! log {
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn get_counter() -> std::slice::Iter {
-    // let item = Counter::new();
-    // item
-    let temp = vec![1, 2, 3, 4, 500];
-    let temp2 = temp.iter();
-    temp2
-    // let temp = item.iter();
-    // let temp2 = item.into_iter();
-    // log!("{}", message);
-
-    // let a = [1, 2, 3];
-    // assert_eq!(a.iter().count(), 3);
-    // for x in &a {
-    //     println!("{}", x);
-    // }
-
-    // for x in item {
-    //     println!("{}", x);
-    // }
-}
-
-#[wasm_bindgen]
 pub struct Counter {
-    count: usize,
-    // vector: Vec<usize>, // todo: what if i don't want a vec?
+    index: usize,
+    data: Vec<char>,
 }
 
+#[wasm_bindgen]
 impl Counter {
-    fn new() -> Counter {
+    pub fn new(data: String) -> Counter {
         Counter {
-            count: 0,
-            // vector: vec![1, 2, 3, 4, 5],
+            index: 0,
+            data: data.chars().collect(),
         }
     }
 }
 
-impl Iterator for Counter {
-    type Item = usize;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.count += 1;
-        log!("test-{}", self.count);
-
-        if self.count < 6 {
-            Some(self.count)
+#[wasm_bindgen]
+impl Counter {
+    pub fn next(&mut self) -> Option<char> {
+        let result = if self.index < self.data.len() {
+            log!("rust_log-{}", self.index);
+            self.data.get(self.index).copied()
         } else {
             None
-        }
+        };
+        self.index += 1;
+        result
     }
 }
-
-// impl IntoIterator for Counter {
-//     type Item = usize;
-//     type IntoIter = std::vec::IntoIter<Self::Item>;
-
-//     fn into_iter(self) -> Self::IntoIter {
-//         self.vector.into_iter()
-//     }
-// }
--- prototypes/wasm-game-of-life/www/index.js
+++ prototypes/wasm-game-of-life/www/index.js
@@ -1,6 +1,32 @@
-//@ts-check
+import { memory } from "wasm-game-of-life/wasm_game_of_life_bg";
+import { Counter } from "wasm-game-of-life";
 
-import * as wasm from "wasm-game-of-life";
+function delay() {
+  return new Promise((res) => {
+    setTimeout(() => {
+      res();
+    }, 3000);
+  });
+}
 
-const counter = wasm.get_counter();
-console.log(counter[4]);
+async function main() {
+  const iterator = Counter.new("hello, world");;
+
+  for (let v; v = iterator.next();) {
+    if (v === "o") {
+      await delay();
+    }
+    console.log("js_log-", v);
+  }
+}
+
+main();
+
+
+// console.log("counter", counter);
+// console.log(new Uint32Array(memory.buffer));
+// console.log(new Uint8Array(memory.buffer, 1114120));
+// // 1114120
+
+// console.log(new Uint8Array(memory.buffer).some(i => i === 34));
+// console.log(new Uint32Array(memory.buffer).some(i => i === 13));
