e4d849fdd83e03059030116504de6008b33ee154
sharpiro <dsharpbb09@gmail.com>
2020-04-10 14:53:12 -0400

    add explanations

--- languages/typescript/unions_and_intersections/index.ts
+++ languages/typescript/unions_and_intersections/index.ts
@@ -1,25 +1,43 @@
-declare function assertType<T>(_item: T): void;
+/**
+ * summary: this does **NOT** directly correlate to intersections/unions in set theory...sigh
+ * when type is A | B then you have access to properties of A AND B (all in common)
+ * when type is A & B then you have access to properties of A OR B (all)
+ */
+
 declare function assertTrue<_T extends true>(): void;
 declare function assertFalse<_T extends false>(): void;
-
-// todo: combine assert and type equal? hard
-type TypeEqual<X, Y> =
+/** Expands `&` types */
+type Evaluate<T> = { [P in keyof T]: T[P] };
+/**
+ * Determines if types `X` and `Y` are equal.  
+ * Since we only have `extends` available, we must instead compare functions to determine equality
+ */
+type TypeEqual<X, Y> = // todo: combine assert and type equal? ...hard, impossible?
   (<T>() => T extends X ? true : false) extends
   (<T>() => T extends Y ? true : false) ? true : false;
+
+/** ands and ors with string literal types */
 
 type AliceFavFoods = "eggs" | "tacos";
 type BobFavFoods = "pasta" | "tacos";
 type AliceOrBobFoods = AliceFavFoods | BobFavFoods;
+assertTrue<TypeEqual<"eggs" | "tacos" | "pasta", AliceOrBobFoods>>();
+
 type AliceAndBobFoods = AliceFavFoods & BobFavFoods;
 type AliceAndBobFoods2 = ("eggs" | "tacos") & ("pasta" | "tacos");
 type AliceAndBobFoods3 = ("eggs" & "pasta") | ("eggs" & "tacos") | ("tacos" & "pasta") | ("tacos" & "tacos");
 type AliceAndBobFoods4 = ("eggs" & "pasta") | ("eggs" & "tacos") | ("tacos" & "pasta") | "tacos";
+type AliceAndBobFoods5 = never | never | never | "tacos";
+type AliceAndBobFoods6 = "tacos";
 
 assertTrue<TypeEqual<AliceAndBobFoods, AliceAndBobFoods2>>();
 assertTrue<TypeEqual<AliceAndBobFoods, AliceAndBobFoods3>>();
 assertTrue<TypeEqual<AliceAndBobFoods, AliceAndBobFoods4>>();
-assertTrue<TypeEqual<"eggs" | "tacos" | "pasta", AliceOrBobFoods>>();
+assertTrue<TypeEqual<AliceAndBobFoods, AliceAndBobFoods5>>();
+assertTrue<TypeEqual<AliceAndBobFoods, AliceAndBobFoods6>>();
 assertTrue<TypeEqual<"tacos", AliceAndBobFoods>>();
+
+/** ands and ors with object types */
 
 type EggsObj = { common: 0, eggs: "eggs"; special: true; };
 type TacosObj = { common: 0, tacos: "tacos", sauce: "marinara"; };
@@ -31,7 +49,7 @@ type AliceOrBobFav = AliceFavObj | BobFavObj;
 
 type AliceAndBobFav = AliceFavObj & BobFavObj;
 type AliceAndBobFav2 = (EggsObj | TacosObj) & (PastaObj | TacosObj);
-type AliceAndBobFav3 = (EggsObj & PastaObj) | (EggsObj & TacosObj) | (TacosObj & PastaObj) | (TacosObj & TacosObj);
+type AliceAndBobFav3 = (EggsObj & PastaObj) | (EggsObj & TacosObj) | (TacosObj & PastaObj) | TacosObj;
 
 assertTrue<TypeEqual<AliceAndBobFav, AliceAndBobFav3>>();
 assertTrue<TypeEqual<AliceAndBobFav, AliceAndBobFav2>>();
@@ -40,10 +58,17 @@ type AliceAndBobFavKeys = keyof AliceAndBobFav;
 assertTrue<TypeEqual<"common", keyof AliceOrBobFav>>();
 assertTrue<TypeEqual<"common" | "sauce", AliceAndBobFavKeys>>();
 
-// unions (OR) give access to properties on both types only until narrowed
 type EggsOrTacosKeys = keyof (EggsObj | TacosObj);
 assertTrue<TypeEqual<"common", EggsOrTacosKeys>>();
 
-// x-sections (AND) give access to all properties on both types
-type EggsAndTacosKeys = keyof (EggsObj & TacosObj);
+type EggsAndTacos = Evaluate<EggsObj & TacosObj>;
+type EggsAndTacosKeys = keyof EggsAndTacos;
 assertTrue<TypeEqual<"eggs" | "tacos" | "common" | "special" | "sauce", EggsAndTacosKeys>>();
+
+/** merging objects with same property name but different type */
+
+type PropType1 = { x: number }
+type PropType2 = { x: string }
+type MergedProp = (PropType1 & PropType2)["x"]
+
+assertTrue<TypeEqual<MergedProp, never>>()
