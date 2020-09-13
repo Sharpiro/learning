c2cad10c2ab0b41d84dac270165e9737ebdd447f
sharpiro <dsharpbb09@gmail.com>
2020-09-12 20:18:32 -0400

    add state

--- prototypes/stdin_streams/src/main.rs
+++ prototypes/stdin_streams/src/main.rs
@@ -1,27 +1,68 @@
 use std::io::{self, Read, Write};
 use std::str;
-// use std::thread;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-  println!("starting stdin_streams");
+  eprintln!("---starting stdin_streams");
+
+  let mut state = State::Open;
   let mut i = 0;
   loop {
     if i > 50 {
       panic!("max loop iterations")
     }
 
-    let mut buffer = [0u8; 100];
+    let mut buffer = [0u8; 1000];
     let bytes_read = io::stdin().read(&mut buffer)?;
     if bytes_read == 0 {
-      io::stdout().write_all(b"done to stdout")?;
+      eprintln!("exited cleanly");
       return Ok(());
     }
     i += 1;
 
     let read_buffer = &buffer[..bytes_read];
-    println!("read: {}", bytes_read);
-    println!("data: {:02x?}", read_buffer);
-    println!("data: {:?}", str::from_utf8(read_buffer)?);
-    // thread::sleep(std::time::Duration::from_secs(5))
+    eprintln!("---read: {}", bytes_read);
+    eprintln!("---data: {:02x?}", read_buffer);
+    eprintln!("---data: {:?}", str::from_utf8(read_buffer)?);
+
+    match state {
+      State::Open => match client_hello(read_buffer) {
+        Ok(data) => {
+          state = State::Initialized;
+          io::stdout().write_all(data)?
+        }
+        Err(err) => io::stdout().write_all(err)?,
+      },
+      State::Initialized => {
+        let data = big_number(read_buffer);
+        io::stdout().write_all(data)?;
+      } //   State::Initialized => match big_number(read_buffer) {
+        //     Ok(data) => {
+        //       state = State::Initialized;
+        //       io::stdout().write_all(data)?
+        //     }
+        //     Err(err) => io::stdout().write_all(err)?,
+        //   },
+        // }
+    }
   }
+}
+
+#[derive(PartialEq)]
+enum State {
+  Open,
+  Initialized,
+}
+
+fn client_hello(buffer: &[u8]) -> Result<&[u8], &[u8]> {
+  if buffer == b"hello\n" {
+    Ok(b"connection initialized\n")
+  } else {
+    Err(b"bad client hello\n")
+  }
+}
+
+// fn big_number(_buffer: &[u8]) -> Result<&[u8], &[u8]> {
+fn big_number(_buffer: &[u8]) -> &[u8] {
+  let _data = &[0, 0, 1, 255, 0x0a];
+  _data
 }
