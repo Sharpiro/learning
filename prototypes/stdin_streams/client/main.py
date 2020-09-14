22832b0cf0d9ef3341cec178f4e5dd29676daebc
sharpiro <dsharpbb09@gmail.com>
2020-09-14 19:31:48 -0400

    start on simple key exchange protocol

--- prototypes/stdin_streams/client/main.py
+++ prototypes/stdin_streams/client/main.py
@@ -1,18 +1,79 @@
 #! /bin/python
 # cspell:disable
 
+import random
+import struct
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("localhost", 8080))
 s.sendall(b"hello\n")
-# buffer = s.recv(1_000)
 print(s.recv(1_000))
-s.sendall(b"get_something\n")
-buffer = s.recv(1_000)[:-1]
-big_num = int.from_bytes(buffer, "big")
-print(len(buffer))
-print(list(buffer))
-print(big_num)
+
+
+# s.sendall(b"get_something\n")
+# buffer = s.recv(1_000)[:-1]
+# big_num = int.from_bytes(buffer, "big")
+# print(len(buffer))
+# print(list(buffer))
+# print(big_num)
 # int.from_bytes()
-# print(int.from_bytes([0, 0, 1, 255], "big"))
+# print(int.from_bytes([1, 0, 1, 255], "big"))
+
+
+def egcd(a, b):
+    if a == 0:
+        return (b, 0, 1)
+    else:
+        gcd, x, y = egcd(b % a, a)
+        _temp = b//a * x
+        return (gcd, y - (b//a) * x, x)
+
+
+def simple_hash_u32(x):
+  buffer = struct.pack(">i", x)
+  return simple_hash(buffer)
+
+
+def simple_hash(buffer):
+  MASK_U32 = 2**32-1
+  hash_address = 5381
+
+  for byte in buffer:
+    hash_address = (((hash_address << 5) & MASK_U32) + hash_address & MASK_U32) + byte 
+    print("ha:", hash_address)
+  return hash_address
+
+
+p = 37
+q = 53
+N = p*q
+phi_N = (p-1)*(q-1)
+e = 17
+d = egcd(e, phi_N)[1]
+x = 1227
+y = 80
+# x = random.randrange(N)
+# y = x**e % N
+k = simple_hash_u32(x)
+# s.sendall(y)
+# print(bytes(812))
+print("x:", x)
+print("y:", y)
+print(list(struct.pack(">i", y)))
+print("k", k)
+s.sendall(struct.pack(">i", y))
+
+
+# def is_prime(n):
+#     skip_map = set()
+#     for i in range(2, n):
+#         # if i in skip_map:
+#         #     continue
+#         quotient, remainder = divmod(n, i)
+#         if remainder == 0:
+#             # skip_map.add(quotient)
+#             print(i)
+
+
+# is_prime(52)
--- prototypes/stdin_streams/src/main.rs
+++ prototypes/stdin_streams/src/main.rs
@@ -1,6 +1,7 @@
+use rand;
+use std::convert::TryInto;
 use std::io::{self, Read, Write};
 use std::str;
-use rand;
 
 #[allow(unused_macros)]
 macro_rules! writeln_buffer {
@@ -32,7 +33,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let read_buffer = &buffer[..bytes_read];
     eprintln!("---read: {}", bytes_read);
     eprintln!("---data: {:02x?}", read_buffer);
-    eprintln!("---data: {:?}", str::from_utf8(read_buffer)?);
+    if let Ok(data) = str::from_utf8(read_buffer) {
+      eprintln!("---data: {:?}", data);
+    }
 
     match state {
       State::Open => match client_hello(read_buffer) {
@@ -45,13 +48,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
       State::Initialized => {
         let data = big_number(read_buffer);
         writeln_buffer!(data)?;
-      } 
-        //     Ok(data) => {
-        //       state = State::Initialized;
-        //       io::stdout().write_all(data)?
-        //     }
-        //     Err(err) => io::stdout().write_all(err)?,
-        //   },
+      }
     }
   }
 }
@@ -64,16 +61,90 @@ enum State {
 
 fn client_hello(buffer: &[u8]) -> Result<&[u8], &[u8]> {
   if buffer == b"hello\n" {
-    Ok(b"connection initialized")
+    Ok(b"hello")
   } else {
     Err(b"bad client hello")
   }
 }
 
-fn big_number(_buffer: &[u8]) -> [u8; 16] {
-  // let temp = u32::from_be_bytes(b"abcd");
+fn big_number(buffer: &[u8]) -> [u8; 16] {
+  // let (split, _) =buffer.split_at(std::mem::size_of::<u32>());
+  let _temp2 = u32::from_be_bytes(buffer.try_into().expect("failure"));
+  eprintln!("---received encrypted value {}", _temp2);
+  let p = 37;
+  let q = 53;
+  let n = 1961;
+  let phi_n = (p - 1) * (q - 1);
+  let e = 17;
+  let (_, d, _) = egcd(e, phi_n);
+  let y = _temp2;
+  let x = pow_mod(y, d as u32, n);
+  let k = simple_hash_u32(x);
+  eprintln!("---decrypted value {}", x);
+  eprintln!("---computed key {}", k);
+  //  let y = x**e%N;
   // let data = 10032_u32.to_be_bytes();
   let data = rand::random();
   // let data = &[0, 0, 1, 255, 0x0a];
   data
+}
+
+fn egcd(a: i32, b: i32) -> (i32, i32, i32) {
+  if a == 0 {
+    return (b, 0, 1);
+  } else {
+    let (gcd, x, y) = egcd(b % a, a);
+
+    let _temp = (b / a) * x;
+    return (gcd, y - (b / a) * x, x);
+  }
+}
+
+fn pow_mod(x: u32, y: u32, n: u32) -> u32 {
+  let mut product = x;
+  for _ in 0..y - 1 {
+    product = product * x % n;
+  }
+  return product;
+}
+
+fn simple_hash_u32(x: u32) -> u32 {
+  let buffer = x.to_be_bytes();
+  simple_hash(&buffer)
+}
+
+fn simple_hash(buffer: &[u8]) -> u32 {
+  const MASK_U32: u64 = std::u32::MAX as u64;
+  let mut hash_address = 5381;
+
+  for &byte in buffer {
+    hash_address = (((hash_address << 5u64) & MASK_U32) + hash_address
+      & MASK_U32)
+      + byte as u64;
+    eprintln!("{}", hash_address);
+  }
+  hash_address as u32
+}
+
+#[test]
+fn egcd_test() {
+  let e = 17;
+  let phi_n = 1872;
+  let (_, d, _) = egcd(e, phi_n);
+  assert!(d == 881);
+}
+
+#[test]
+fn hash_test() {
+  let buffer = [0, 1, 2, 3, 4];
+  let hash = simple_hash(&buffer);
+  println!("{}", hash);
+  assert!(hash == 134190447)
+}
+
+#[test]
+fn hash_test_num() {
+  let x = 1227;
+  let hash = simple_hash_u32(x);
+  println!("{}", hash);
 }
