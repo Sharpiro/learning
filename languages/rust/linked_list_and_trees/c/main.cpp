b201cf9f39d1e55f0b4c714cd446b276c2f91110
sharpiro <dsharpbb09@gmail.com>
2022-02-07 16:35:13 -0500

    functional c++ "concept"

--- languages/rust/linked_list_and_trees/c/main.cpp
+++ languages/rust/linked_list_and_trees/c/main.cpp
@@ -1,11 +1,15 @@
 #include <stdio.h>
-// #include "./types.h"
+#include "./types.h"
 #include "./vec.h"
 // #include "test.h"
 // #include <stdbool.h>
 // #include <unistd.h>
 // #include "node.h"
 // #include "temp.h"
+#include <string>
+#include <cstddef>
+#include <concepts>
+#include <iostream>
 
 // void test_to_vec()
 // {
@@ -22,21 +26,70 @@
 //   assert_eq(vec.raw[4], 5);
 // }
 
+// template <typename T, typename U>
+// concept SomeConcept = std::is_base_of<U, T>::value;
+struct SimpleStruct
+{
+  int x;
+};
+
+struct MyStruct
+{
+  int x;
+  double y;
+  bool z;
+};
+
+void do_something(MyStruct &book)
+{
+  book = MyStruct{.x = 88};
+  // book.x = 99;
+  //
+}
+
+struct BadStruct
+{
+  int f;
+};
+
+template <typename T>
+concept Debug = requires(T a)
+{
+  {
+    a.x
+    } -> std::convertible_to<int>;
+};
+
+template <Debug T>
+void format_debug(T *my_struct)
+{
+  // std::cout << "hi????\n";
+  std::cout << 1;
+  // auto temp = my_struct->myFunc();
+  const auto [x, y, z] = *my_struct;
+  printf("{ x: %d, y: %f, z: %d }\n", x, y, z);
+  // printf("{ x: %d, y: %f, z: %d }\n", my_struct->x, my_struct->y, my_struct->z);
+}
+
 int main()
 {
   // int x = divide(100, 21);
   // double y = divide(100.0, 21.0);
   // printf("%d\n", x);
 
-  // Vec vec = {};
-  Vec vec = vec_new();
-  int temp = vec.temp();
-  int temp_static = Vec::temp_static();
-  printf("%d\n", temp);
-  printf("%d\n", temp_static);
-  vec_push(&vec, 1);
-  vec_push(&vec, 2);
-  print_vec(&vec);
+  // Vec<u8> vec = Vec<u8>::new_vec();
+  // vec.push(1);
+  // vec.print_debug();
+
+  // auto vec = Vec<MyStruct>::new_vec();
+  // auto my_struct = MyStruct{.x = 100, .y = 2.0, .z = true};
+  auto my_struct = MyStruct{.x = 99};
+  do_something(my_struct);
+  // auto my_struct = BadStruct{};
+  format_debug(&my_struct);
+  // vec.push(my_struct);
+  // print_my_struct(&my_struct);
+  // vec.print_debug();
   // test_to_vec();
   // Node root = get_test_list(5, 1);
   // print_list(&root);
--- languages/rust/linked_list_and_trees/c/vec.h
+++ languages/rust/linked_list_and_trees/c/vec.h
@@ -1,23 +1,67 @@
 #pragma once
 
-#include "./types.h"
+#include <stdlib.h>
+#include <string.h>
+// #include "./types.h"
 
-// template <typename T>
+template <typename T>
 struct Vec
 {
-  u8 *raw;
-  // T *raw;
-  u8 len;
-  u8 capacity;
-  int temp();
-  static int temp_static();
+  // u8 *raw;
+  T *raw;
+  int len;
+  int capacity;
+  void push(T val);
+  void append(Vec<T> *other_vec);
+  void print_debug();
+  static Vec<T> new_vec();
 };
 
-// template <typename T>
-Vec vec_new();
+template <typename T>
+void Vec<T>::push(T val)
+{
+  if (this->len == this->capacity)
+  {
+    int new_capacity = this->capacity * 2;
+    T *new_raw = (T *)malloc(new_capacity);
+    memcpy(new_raw, this->raw, this->len);
+    free(this->raw);
+    this->raw = new_raw;
+    this->capacity = new_capacity;
+    // printf("expanding vec to: %d\n", new_capacity);
+  }
 
-// template <typename T>
-void vec_push(Vec *vec, u8 val);
+  // printf("adding %d to vec\n", val);
+  int index = this->len;
+  this->raw[index] = val;
+  this->len++;
+}
 
-// template <typename T>
-void print_vec(Vec *vec);
+template <typename T>
+Vec<T> Vec<T>::new_vec()
+{
+  int default_capacity = 2;
+  T *raw = (T *)malloc(default_capacity);
+  Vec<T> vec = {.raw = raw, .capacity = default_capacity};
+  return vec;
+}
+
+template <typename T>
+void Vec<T>::append(Vec<T> *other_vec)
+{
+  for (int i = 0; i < other_vec->len; i++)
+  {
+    this->push(other_vec->raw[i]);
+  }
+}
+
+template <typename T>
+void Vec<T>::print_debug()
+{
+  printf("[");
+  for (int i = 0; i < this->len; i++)
+  {
+    printf("%d,", this->raw[i]);
+  }
+  puts("]");
+}
